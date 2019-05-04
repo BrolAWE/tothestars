4549d46a9941a1bb77fd6e109708315b0335a552
Alexey Delov <aleksejdelov@gmail.com>
2019-05-04 20:59:48 +0300

    Core

--- tothestars/settings.py
+++ tothestars/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import dj_database_url
+from decouple import config
 import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
