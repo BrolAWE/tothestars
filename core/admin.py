1e51cfe09f6a810cbb575cd8410558185e123c46
Alexey <aleksejdelov@gmail.com>
2019-05-08 18:37:36 +0300

    xxx

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import Kite
+
+admin.site.register(Kite)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Kite(models.Model):
+    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
