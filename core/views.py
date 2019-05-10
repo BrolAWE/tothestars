b163fe8010ea9e9de7ebb71d3019d29cb64a42b2
Alexey <aleksejdelov@gmail.com>
2019-05-10 20:59:21 +0300

    xxx

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
+import json
+
 from django.shortcuts import render
 
-
 # Create your views here.
+from core.models import Kite
+
 
 def index(request):
-    return render(request, 'index.html')
+    kites = Kite.objects.all()
+    return render(request, 'index.html', context={'kites': kites})
